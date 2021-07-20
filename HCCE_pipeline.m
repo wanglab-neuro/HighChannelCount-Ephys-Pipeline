168e501164f9dc8c85394ede6f64b78e3e75de3c
Vincent <prevosto@mit.edu>
2021-07-20 13:37:18 -0400

    Update HCCE_pipeline.m
    changed HCCE.m file as a function

--- HCCE_pipeline.m
+++ HCCE_pipeline.m
@@ -1,9 +1,14 @@
-% start from the folder where the files to process are
+function HCCE_pipeline(exportNotes, exportData, spikeSort)
+% Runs the blackrock pipline with analysis options of your choice.
+%   exportNotes: boolean 
+%   exportData: boolean
+%   spikeSort: boolean
 dataDir = cd;
 [filepath,folderName] = fileparts(dataDir);
 
+
 %% Export notes
-exportNotes = 0;
+%exportNotes = 0;
 if exportNotes
     % list directories above, assuming the parent folder is the container for
     % all files from that subject
@@ -13,14 +18,17 @@ if exportNotes
 end
 
 %% Export .dat files with BatchExport
-exportData = 0;
+%exportData = 0;
 if exportData
     % start from data session's root directory
     [dataFiles,allRecInfo]=BatchExport;
     save('fileInfo','dataFiles','allRecInfo');
 end
 
-spikeSort = 1;
+%spikeSort = 1;
 if spikeSort
     BatchSpikeSort_KS_JRC(dataDir,folderName,allRecInfo)
+end
+
+
 end
