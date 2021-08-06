663e382dff2fb403cc956d00088869a7d459d0c3
Vincent Prevosto <prevosto@mit.edu>
2021-08-05 21:25:57 -0400

    Update SplitPSTH.m

--- DataVisualization/SplitPSTH.m
+++ DataVisualization/SplitPSTH.m
@@ -1,12 +1,12 @@
-function SplitPSTH(rasters,groups,conditions,options)
-
+function sdf=SplitPSTH(rasters,groups,conditions,options)
+figure('Position',[1050 120 750 790],'Color','w');
 if contains(options,'rasters')
-    figure('Position',[1050 120 750 790],'Color','w');
+    
     %% plot rasters
     colormap(flipud(gray));
     imagesc(logical(rasters));
     xlabel('Time (ms)');
-    ylabel('Trial#');
+    ylabel('Condition #');
     set(gca,'XTick',(0:1000:size(rasters,2))+0.5); %0:100:preAlignWindow+200); %'XLim',[preAlignWindow-250.5 preAlignWindow+250.5]
     set(gca,'XTickLabel',(0:size(rasters,2)/1000)); %'FontSize',10,'FontName','calibri'
     set(gca,'FontSize',10,'FontName','Calibri','TickDir','out');
@@ -19,29 +19,28 @@ if contains(options,'rasters')
     
     box off; % axis tight
     % title(figureOptions.legends{3});
-end
-if contains(options,'PSTH')
-    figure('Position',[1050 120 750 790],'Color','w');
+elseif contains(options,'Bars')% bar plots
+    barPlot=bar(movmean(mean(rasters),5));
+    %     barPlot.FaceColor=[0.1 0.4 0.8];
+    barPlot.EdgeColor='none';
+    barPlot.BarWidth=1;
+elseif contains(options,'PSTH')
     hold on;
+    cmap=lines;
     %% plot PSTHs
     for conditionNum=1:numel(unique(groups))
-        %         bar plots
-        %         barPlot=bar(movmean(mean(rasters),5));
-        %         %     barPlot.FaceColor=[0.1 0.4 0.8];
-        %         barPlot.EdgeColor='none';
-        %         barPlot.BarWidth=1;
-        cmap=lines;
+        
         groupIdx=groups==conditionNum;
         %% plot sdf
         conv_sigma=20;
-        [sdf, ~, rastsem]=conv_raster(rasters(groupIdx,:),conv_sigma,1);
-       
+        [sdf, ~, rastsem]=conv_raster(rasters(groupIdx,:),conv_sigma,0);
+        
         %plot sem
-%         patch([1:length(sdf),fliplr(1:length(sdf))],[sdf-rastsem,fliplr(sdf+rastsem)],...
-%             'k','EdgeColor','none','FaceAlpha',0.2); 
+        %         patch([1:length(sdf),fliplr(1:length(sdf))],[sdf-rastsem,fliplr(sdf+rastsem)],...
+        %             'k','EdgeColor','none','FaceAlpha',0.2);
         %plot sdfs
         plot(gca,sdf,'Color',cmap(conditionNum,:),'LineWidth',1.5);
-                
+        
         xlabel('Time (ms)');
         ylabel('Mean Firing Rate (spikes/s)');
         set(gca,'XTick',(0:1000:size(rasters,2))+0.5); %0:100:preAlignWindow+200); %'XLim',[preAlignWindow-250.5 preAlignWindow+250.5]
@@ -50,10 +49,36 @@ if contains(options,'PSTH')
         set(gca,'FontSize',10,'FontName','Calibri','TickDir','out');
         
         box off;
-        
-        legend
-        
+              
     end
+    legend(conditions)
+    
+elseif contains(options,'Heatmap')
+    conv_sigma=20;
+    sdf=nan(numel(unique(groups)),5000-(conv_sigma*6));
+    for conditionNum=1:numel(unique(groups))
+        groupIdx=groups==conditionNum;
+        sdf(conditionNum,:)=conv_raster(rasters(groupIdx,:),conv_sigma);
+    end
+    colormap(hot);
+    imagesc(sdf)
+    xlabel('Time (s)');
+    ylabel('Wall Distance (mm)');
+    set(gca,'XTick',(-conv_sigma*3:1000:size(rasters,2))+0.5); %0:100:preAlignWindow+200); %'XLim',[preAlignWindow-250.5 preAlignWindow+250.5]
+    set(gca,'XTickLabel',(0:size(rasters,2)/1000)); %'FontSize',10,'FontName','calibri'
+    set(gca,'YTick',1:conditionNum,'YTickLabel',unique(conditions))
+    set(gca,'FontSize',10,'FontName','Calibri','TickDir','out');
+    cbh=colorbar;
+    ylabel(cbh,'Mean Firing Rate (spikes/s)');
+
+    % draw alignment bar
+    % currylim=get(gca,'YLim');
+    % patch([repmat(figureOptions.alignSpecs{1},1,2) repmat(figureOptions.alignSpecs{1}+figureOptions.alignSpecs{3},1,2)], ...
+    %     [[0 currylim(2)] fliplr([0 currylim(2)])], ...
+    %     [0 0 0 0],figureOptions.alignSpecs{4},'EdgeColor','none','FaceAlpha',0.3);
+    
+    box off; % axis tight
+    
 end
 end
 
