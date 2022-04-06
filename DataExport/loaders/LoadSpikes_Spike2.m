67d8e1afc5c44222ae2df55537a50b28cd85a079
Vincent <prevosto@mit.edu>
2022-04-06 12:06:28 -0400

    Update LoadSpikes_Spike2.m

--- DataExport/loaders/LoadSpikes_Spike2.m
+++ DataExport/loaders/LoadSpikes_Spike2.m
@@ -6,5 +6,10 @@ spikes.times=data.(flNames{1}).times;
 spikes.waveforms=data.(flNames{1}).values;
 spikes.timebase=1;
 spikes.samplingRate=1/data.(flNames{1}).interval;
-spikes.preferredElectrode=ones(numel(spikes.unitID),1);
+if logical(regexp(fName,'Ch\d+.'))
+    chNum=str2double(regexp(fName,'(?<=Ch)\d+(?=\.)','match','once'));
+else
+    chNum=1;
+end
+spikes.preferredElectrode=ones(numel(spikes.unitID),1)*chNum;
 end
