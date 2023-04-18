64e701d24bfda39347e8074f0d1371e6ab63ed6e
Vincent Prevosto <prevosto@openmind7.ib.cluster>
2023-04-18 14:57:24 -0400

    SI proc sort py script update

--- scripts/si_proc_sort.py
+++ scripts/si_proc_sort.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 # Run this script as follow:
-# python combined_script.py "path\to\data" 30
+# python si_proc_sort.py "path\to\data" 30
 
 # Import libraries
 import spikeinterface as si
@@ -30,12 +30,14 @@ args = parser.parse_args()
 # Define the base folder
 base_folder = Path(args.base_folder)
 # base_folder = Path(r"/scratch2/weka/wanglab/prevosto/data/sc012/sc012_0123/sc012_0123_001")
+# base_folder = Path(r"/scratch2/weka/wanglab/prevosto/data/sc012/sc012_0120/sc012_0120_002")
+# base_folder = Path(r"/scratch2/scratch/Wed/vincent/whisker_asym/sc014/sc014_0324/sc014_0324_001")
 # base_folder = Path(r"D:\Vincent\Data\sc014\sc014_0324\sc014_0324_001")
 file_path = base_folder.joinpath("Record Node 101")
 
 # Set sorter parameters
 sorter_params = dict(n_jobs=args.n_jobs)
-# sorter_params = dict(n_jobs=30)
+# sorter_params = dict(n_jobs=80)
 
 # Read the OpenEphys file
 recording = se.read_openephys(file_path, stream_id='1')
@@ -55,13 +57,18 @@ print(f"Number of segments: {num_segments}")
 # Phase shift
 recording = spre.phase_shift(recording)
 
+# Keep only first segment (baseline)
+recording_baseline = si.SelectSegmentRecording(recording, segment_indices=0)
+# recording_baseline = recording
+
 # High-pass filter
-recording_f = spre.highpass_filter(recording, freq_min=300)
-# recording = spre.bandpass_filter(recording, freq_min=300, freq_max=6000)
+# recording_f = spre.highpass_filter(recording, freq_min=300)
+recording_f = spre.bandpass_filter(recording_baseline, freq_min=300, freq_max=10000)
+recording_f.annotate(is_filtered=True)
 
 # Common Median Reference
-recording_baseline = si.SelectSegmentRecording(recording_f, segment_indices=0)
-recording_b_cmr = spre.common_reference(recording_baseline, reference='global', operator='median')
+recording_b_cmr = spre.common_reference(recording_f, reference='global', operator='median')
+# recording_b_cmr = spre.common_reference(recording_f, reference='global', operator='median')
 
 # Job settings for saving the preprocessed recording
 job_kwargs = dict(n_jobs=10, chunk_duration="1s", progress_bar=True)
@@ -73,21 +80,37 @@ recording_saved = recording_b_cmr.save(folder=base_folder / "preprocessed", form
 # Load the preprocessed recording
 # recording_saved = ZarrRecordingExtractor(root_path=zarr_folder)
 
+# recording_concat = si.concatenate_recordings([recording_b_cmr])
+
 # Run spike sorting using PyKilosort
-sorting_pyKS = ss.run_pykilosort(recording_saved,
-                                 output_folder=base_folder / 'results_pyKS',
-                                 verbose=True, docker_image="spikeinterface/pykilosort-base:latest",
-                                 **sorter_params)
+# sorting_pyKS = ss.run_pykilosort(recording_saved,
+#                                  output_folder=base_folder / 'results_pyKS',
+#                                  verbose=True, singularity_image="spikeinterface/pykilosort-base:latest",
+#                                  **sorter_params)
+
+# Run spike sorting using Kilosort3
+# sorting_KS3 = ss.run_kilosort3(recording_b_cmr,
+#                                  output_folder=base_folder / 'results_KS3',
+#                                  verbose=True, singularity_image=True)
+
+# Run spike sorting using TriDesClous
+sorting_TDC = ss.run_tridesclous(recording_saved,
+                            output_folder=base_folder / 'results_TDC',
+                            verbose=True, **sorter_params) 
 
 # Extract waveforms
-job_kwargs = dict(n_jobs=30, chunk_duration="1s", progress_bar=False)
-we_all = si.extract_waveforms(recording_saved, sorting_pyKS, folder=base_folder / "waveforms_all", 
-                              max_spikes_per_unit=None,
-                              overwrite=True,
-                              **job_kwargs)
+# job_kwargs = dict(n_jobs=30, chunk_duration="1s", progress_bar=False)
+we = si.extract_waveforms(recording_saved, sorting_TDC, folder=base_folder / "waveforms", 
+                          load_if_exists=False, overwrite=True, **job_kwargs)
+# print(we)
+
+# we_all = si.extract_waveforms(recording_saved, sorting_pyKS, folder=base_folder / "waveforms_all", 
+#                               max_spikes_per_unit=None,
+#                               overwrite=True,
+#                               **job_kwargs)
 
 # Print waveform information for each unit
-for unit in sorting_pyKS.get_unit_ids():
-    waveforms = we_all.get_waveforms(unit_id=unit)
-    spiketrain = sorting_pyKS.get_unit_spike_train(unit)
+for unit in sorting_TDC.get_unit_ids():
+    waveforms = we.get_waveforms(unit_id=unit)
+    spiketrain = sorting_TDC.get_unit_spike_train(unit)
     print(f"Unit {unit} - num waveforms: {waveforms.shape[0]} - num spikes: {len(spiketrain)}")
